3d62239246ac88138d0ae63f9b08999f66d25f3d
dabid chu <arranzd5@gmail.com>
2025-11-11 02:24:47 +0100

    dalee

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+/*
         //Temperaturas diarias: Recibe temperaturas diarias de un mes. Encuentra la racha más larga
         // de días consecutivos con temperatura superior a 30°C.
 
@@ -37,7 +37,7 @@ public class Main {
                 if (contadorPar > contadorParMax) {
                     contadorParMax = contadorPar;
                 }
-            }else  {
+            } else {
                 contadorPar = 0;
             }
         }
@@ -48,17 +48,17 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite um numero: (hasta que pongas unn 0 para salair");
-        int x= 0;
+        int x = 0;
         int numero;
         int[] numerostemp = new int[100];
-        do{
+        do {
             System.out.println("numero ");
             numero = sc.nextInt();
-            if(numero != 0){
+            if (numero != 0) {
                 numerostemp[x] = numero;
                 x++;
             }
-        }while (numero != 0);
+        } while (numero != 0);
 
         int secuencia = 0;
         int secuenciaMax = 3;
@@ -66,16 +66,64 @@ public class Main {
         int[] numeros = new int[x];
         for (int i = 0; i < x; i++) {
             numeros[i] = numerostemp[i];
-            if(numeros[i] % 2 == 0){
+            if (numeros[i] % 2 == 0) {
                 secuencia++;
-                if(secuencia > secuenciaMax){
+                if (secuencia > secuenciaMax) {
                     secuenciaMax = secuencia;
                 }
-            }else {
+            } else {
                 secuencia = 0;
             }
         }
 
         System.out.println(secuenciaMax);
+
+
+
+        //RepasoSuperFinal
+        //Ejercicio 1
+        //Crea un programa que llene una matriz de 2x2 con números ingresados por
+        //el usuario y luego imprima todos los elementos.
+        Scanner sp = new Scanner(System.in);
+
+        int[][] numeros1 = new int[2][2];
+        System.out.println("pon numeors para rellenar la matriz");
+
+        for (int i = 0; i < numeros1.length; i++) {
+            for (int j = 0; j < numeros1[0].length; j++) {
+                numeros1[i][j] = sp.nextInt();
+            }
         }
+        //Para representar la matriz en un print
+        for (int i = 0; i < numeros1.length; i++) {
+            for (int j = 0; j < numeros1.length; j++) {
+                System.out.print(numeros1[i][j] + " ");
+            }
+            System.out.println();
+
+ */
+
+            //Ejercicio2
+            //Crea una función sumMatrix() que sume los elementos de una matriz y
+            //devuelva la suma.
+
+            int[][] sumaMatrix ={
+                    {1,2,6,5},
+                    {3,5,4,3},
+                    {7,4,3,2}
+            };
+
+            System.out.println(sumMatrix(sumaMatrix));
+        }
+
+    public static int sumMatrix(int[][] matrix) {
+
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                sum = sum + matrix[i][j];
+            }
+        }
+        return sum;
     }
+}
